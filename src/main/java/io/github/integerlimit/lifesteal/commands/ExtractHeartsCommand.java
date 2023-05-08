d1abbf04bb87caee20e20502c12f35f3ce098f17
IntegerLimit <isaacwonglh111@gmail.com>
2023-05-08 14:24:39 +1000

    Remove useless import

--- src/main/java/io/github/integerlimit/lifesteal/commands/ExtractHeartsCommand.java
+++ src/main/java/io/github/integerlimit/lifesteal/commands/ExtractHeartsCommand.java
@@ -10,7 +10,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import org.checkerframework.checker.units.qual.C;
 
 import java.util.ArrayList;
 import java.util.List;

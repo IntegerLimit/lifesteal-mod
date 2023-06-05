d8b5cba140432c055ef39025f8c05febe90599b6
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-06-05 18:33:58 +1000

    Revive Block Mark II: Textures
    It looks like a beacon but with hearts and redstone block instead of obsidian
    
    All texture options in the poll are currently in this commit. The setting is set to Option E currently.

--- src/main/java/io/github/integerlimit/lifesteal/blocks/ReviveBlock.java
+++ src/main/java/io/github/integerlimit/lifesteal/blocks/ReviveBlock.java
@@ -1,13 +1,11 @@
 package io.github.integerlimit.lifesteal.blocks;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
-
 public class ReviveBlock extends Block {
     public ReviveBlock() {
         super(BlockBehaviour.Properties.of(Material.METAL)
-                .sound(SoundType.METAL).strength(6f));
+                .sound(SoundType.METAL).strength(0.5f).explosionResistance(1000f).noOcclusion());
     }
 }
